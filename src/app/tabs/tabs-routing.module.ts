40e90a7f1498d6b665204436be822e4e4932f582
Brandon de Jesús Lara Ruiz <74059945+BrandonJLR-UTAGS@users.noreply.github.com>
2023-03-06 20:38:10 -0600

    navega de nuevo

--- src/app/tabs/tabs-routing.module.ts
+++ src/app/tabs/tabs-routing.module.ts
@@ -23,24 +23,24 @@ const routes: Routes = [
         path: 'tab4',
         loadChildren: () => import('../tab4/tab4.module').then(m => m.Tab4PageModule)
       },
-      // {
-      //   path: 'cronograma',
-      //   loadChildren: () => import('../cronograma/cronograma.module').then(m => m.CronogramaPageModule)
-      // },
-      // {
-      //   path: 'proyectos',
-      //   loadChildren: () => import('../proyectos/proyectos.module').then(m => m.ProyectosPageModule)
-      // },
+      {
+        path: 'cronograma',
+        loadChildren: () => import('../cronograma/cronograma.module').then(m => m.CronogramaPageModule)
+      },
+      {
+        path: 'proyectos',
+        loadChildren: () => import('../proyectos/proyectos.module').then(m => m.ProyectosPageModule)
+      },
       {
         path: '',
-        redirectTo: '/tabs/tab1',
+        redirectTo: 'tabs/tab1',
         pathMatch: 'full'
       }
     ]
   },
   {
     path: '',
-    redirectTo: '/tabs/tab1',
+    redirectTo: 'tabs/tab1',
     pathMatch: 'full'
   }
 ];
