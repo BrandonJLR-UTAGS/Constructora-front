fa4e2163fbabc76101966cbb77370ddc44a87522
Brandon de Jesús Lara Ruiz <74059945+BrandonJLR-UTAGS@users.noreply.github.com>
2023-03-17 13:52:20 -0600

    Merge Cambios

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,58 +1,5 @@
 
-import { NgModule } from '@angular/core';
-import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [
-  {
-    path: '',
-    redirectTo: 'login',
-    pathMatch: 'full',
-
-  },
-  {
-    path: 'cronograma',
-    loadChildren: () => import('./cronograma/cronograma.module').then( m => m.CronogramaPageModule),
-
-  },
-  {
-    path: 'login',
-    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
-  },
-  {
-    path: 'login',
-    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
-  },
-  {
-    path: 'registro-empleados',
-    loadChildren: () => import('./registro-empleados/registro-empleados.module').then( m => m.RegistroEmpleadosPageModule)
-  },
-  {
-    path: 'proyectos',
-    loadChildren: () => import('./proyectos/proyectos.module').then( m => m.ProyectosPageModule)
-  },
-  {
-    path: 'inicio',
-    loadChildren: () => import('./tabs/tabs.module').then( m => m.TabsPageModule)
-  },
-  {
-    path: 'registro-proyectos',
-    loadChildren: () => import('./registro-proyectos/registro-proyectos.module').then( m => m.RegistroProyectosPageModule)
-  },
-  {
-    path: 'resumen-proyecto',
-    loadChildren: () => import('./resumen-proyecto/resumen-proyecto.module').then( m => m.ResumenProyectoPageModule)
-  }
-
-
-];
-@NgModule({
-  imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
-  ],
-  exports: [RouterModule]
-})
-export class AppRoutingModule {}
-=======
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
@@ -94,6 +41,10 @@ const routes: Routes = [
   {
     path: 'actividad',
     loadChildren: () => import('./actividad/actividad.module').then( m => m.ActividadPageModule)
+  },
+  {
+    path: 'resumen-proyecto',
+    loadChildren: () => import('./resumen-proyecto/resumen-proyecto.module').then( m => m.ResumenProyectoPageModule)
   }
 
 
